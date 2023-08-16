362eb8f415ba37890edbf1b284eafb2247124c6b
ankeeta431997 <ankeeta431997@gmail.com>
2023-08-16 11:39:51 +0530

    test case

--- src/components/buttons/buttons.test.js
+++ src/components/buttons/buttons.test.js
@@ -5,5 +5,31 @@ import BaseButton from './BaseButton';
 
 describe('BaseButton Component', () => {
  
-
+    it('renders button label', () => {
+        const label = 'Click Me';
+        const { getByText } = render(<BaseButton label={label} />);
+        const buttonElement = getByText(label);
+        expect(buttonElement).toBeInTheDocument();
+      });
+    
+      it('applies className and id props', () => {
+        const className = 'custom-button';
+        const id = 'test-button';
+        const label = 'Click Me';
+        const { getByText } = render(<BaseButton label={label} className={className} id={id} />);
+        const buttonElement = getByText(label);
+        expect(buttonElement).toHaveClass(className);
+        expect(buttonElement).toHaveAttribute('id', id);
+      });
+      
+      it('calls onClick prop when button is clicked', () => {
+        const onClickMock = jest.fn();
+        const label = 'Click Me';
+        const { getByText } = render(<BaseButton label={label} onClick={onClickMock} />);
+        const buttonElement = getByText(label);
+    
+        fireEvent.click(buttonElement);
+    
+        expect(onClickMock).toHaveBeenCalled();
+      });
 });
